368fa93195814167e38ff9ea6f2b7f753b583e92
William Frazier <will.frazier@gmail.com>
2025-06-11 16:43:55 -0600

    adds expired and timeout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::net::{Ipv4Addr, IpAddr, SocketAddrV4, UdpSocket, SocketAddr};
-use std::sync::mpsc;
+use std::sync::mpsc::{self, Sender};
 use std::thread;
 use std::time::{Duration, Instant};
+use std::collections::HashMap;
 
 enum Message {
     AddMatchRequest(MatchRequest),
@@ -21,6 +22,10 @@ impl MatchRequest {
       created_at: Instant::now(),
       source: source,
     }
+  }
+
+  fn expired(&self) -> bool {
+    self.created_at > Instant::now() + Duration::from_secs(120)
   }
 }
 
@@ -43,7 +48,7 @@ fn main() {
       if let SocketAddr::V4(v4_socket) = src {
         if let Ok(id) = String::from_utf8(buf[..amt].to_vec()) {
           let match_request = MatchRequest::build(id, v4_socket);
-          tx1.send(Message::AddMatchRequest(match_request)).unwrap()
+          tx1.send(Message::AddMatchRequest(match_request)).expect("tx2 send failed");
         }
       }
     }
@@ -63,37 +68,38 @@ fn main() {
     ];
 
     for match_request in match_requests {
-      tx2.send(Message::AddMatchRequest(match_request)).unwrap();
+      tx2.send(Message::AddMatchRequest(match_request)).expect("tx2 send failed");
       thread::sleep(Duration::from_millis(2000))
     }
   });
   
-  
-
-    // thread::spawn(move || {
-    //     let vals = vec![
-    //         String::from("more"),
-    //         String::from("messages"),
-    //         String::from("for"),
-    //         String::from("you"),
-    //     ];
-
-    //     for val in vals {
-    //         tx.send(val).unwrap();
-    //         thread::sleep(Duration::from_secs(1));
-    //     }
-    // });
-
-    for received in rx {
-      match received {
-        Message::AddMatchRequest(match_request) => {
-          println!("Add match request triggered");
-        },
-        Message::TriggerTimeouts => {
-          println!("timeout check triggered");
-        },
-      }
+  // timeout trigger thread
+  let tx3 = tx.clone();
+  thread::spawn(move || {
+    loop {
+      thread::sleep(Duration::from_secs(15));
+      tx3.send(Message::TriggerTimeouts).expect("tx3 send failed");
     }
+  });
+    
+  
+  // *****************************************
+  // Main loop to handle the thread actions
+  
+  let mut match_requests: HashMap<String, MatchRequest> = HashMap::new();
+
+  for received in rx {
+    match received {
+      Message::AddMatchRequest(match_request) => {
+        println!("Add match request triggered, {:?}", match_request.id);
+        match_requests.insert(match_request.id.clone(), match_request);
+      },
+      Message::TriggerTimeouts => {
+        println!("timeout check triggered");
+        match_requests.retain(|_, v| !v.expired())
+      },
+    }
+  }
 
 
 }
